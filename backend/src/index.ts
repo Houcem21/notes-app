9aa7028c1cb93cc0719ed379eecddc6775243798
Houssem <Houssem@Diverta0057s-MacBook-Pro.local>
2025-08-05 14:35:57 +0900

    routing-setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
-// import PocketBase from 'pocketbase';
+import PocketBase from 'pocketbase';
+import express, {Application, Request, Response} from 'express';
 
-// const pb = new PocketBase('http://127.0.0.1:8090');
+const app : Application = express();
+const port : number = 3000;
+const pb = new PocketBase('http://127.0.0.1:8090');
 
-// const userData = await pb.collection('users').authWithPassword('test@example.com', '12345678');
-
-// console.log(userData);
+const userData = await pb.collection('users').authWithPassword('test@example.com', '12345678');
+console.log(userData);
 
 
 // C operation
@@ -29,3 +31,18 @@
 // await pb.collection('notes').delete('s4j4ebi36i67z74');
 
 // set up express server
+
+app.use(express.json());
+
+app.get('/note-made', async (req: Request,res: Response) => {
+    try {
+        const record = await pb.collection('notes').create({title: "Einkaufslist",content: "Milch und Brot"})
+        res.json(record);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch users' });
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+})
