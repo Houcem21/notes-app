51305347adcd04f8993238450b0b1b628d8459ff
Houssem <Houssem@Diverta0057s-MacBook-Pro.local>
2025-08-05 20:13:30 +0900

    basic-routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,12 +34,55 @@ console.log(userData);
 
 app.use(express.json());
 
-app.get('/note-made', async (req: Request,res: Response) => {
+app.get('/', async (req: Request,res: Response) => {
     try {
-        const record = await pb.collection('notes').create({title: "Einkaufslist",content: "Milch und Brot"})
-        res.json(record);
+        res.send('This is the homepage, pick a route <br> /r /c /d /u')
     } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch users' });
+        res.status(500).json({ error: 'Failed to find home' });
+    }
+})
+
+app.get('/r', async (req: Request,res: Response) => {
+    try {
+        const records = await pb.collection('notes').getFullList({
+            sort: '-created',
+        });
+        console.log(records)
+        res.status(200).send(records)
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to read all' });
+    }
+})
+
+app.post('/c', async (req: Request,res: Response) => {
+    try {
+        const {title, content} = req.body;
+        const record = await pb.collection('notes').create({title: title ,content: content});
+
+        res.status(201).send(record)
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to create note' });
+    }
+})
+
+app.put('/u', async (req: Request,res: Response) => {
+    try {
+        const {id, title, content} = req.body;
+        const record = await pb.collection('notes').update(id ,{title: title ,content: content});
+
+        res.status(201).send(record)
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update note' });
+    }
+})
+
+app.delete('/d', async (req: Request,res: Response) => {
+    try {
+        const {id} = req.body;
+        await pb.collection('notes').delete(id);
+        res.status(201).send("Successfully deleted " + id)
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete note' });
     }
 })
 
